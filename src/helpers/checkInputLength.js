d31ed47d509b087beaafb560aa982bbb554c8b75
AlexeyGr <a.i.grishechkin@gmail.com>
2021-06-13 15:01:37 +0300

    name refactoring in checkInputLength function

--- src/helpers/checkInputLength.js
+++ src/helpers/checkInputLength.js
@@ -1,6 +1,6 @@
-export const checkInputLength = (name, params) => {
-  if (name.length === 0) return `Field "${params.name}" must not be empty`;
+export const checkInputLength = (input, params) => {
+  if (input.length === 0) return `Field "${params.name}" must not be empty`;
   return (
-    name.length < params.length && `Field "${params.name}" must be more ${params.length} characters`
+    input.length < params.length && `Field "${params.name}" must be more ${params.length} characters`
   );
 };
